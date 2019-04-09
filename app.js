1693f206554a5a8fe239217db18006117a0c21e7
Rider Jensen <riderjensen@gmail.com>
2019-04-09 14:13:09 -0600

    add all headers

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Methods', 'GET POST DELETE PUT');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
