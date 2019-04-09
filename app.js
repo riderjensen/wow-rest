af0dade8b7298412570d957b4d3838d3f2b3f0b0
Rider Jensen <riderjensen@gmail.com>
2019-04-09 11:57:48 -0600

    fix sort function

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/edit', editRoute);
 app.use('/find', findRoute);
 
 app.get('/', (req, res, next) => {
-	mountModel.find().sort([['name', 'desencing']]).then(response => {
+	mountModel.find({},null, {sort: {name: 1}}).then(response => {
 		res.status(200).send(response);
 	})
 });
