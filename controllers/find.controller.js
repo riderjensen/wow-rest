62de98c30c5f508661ad4a95ced6db33634fb6df
rider <Riderjensen@gmail.com>
2019-04-08 19:01:02 -0600

    Adding routes and controllers

--- controllers/find.controller.js
+++ controllers/find.controller.js
@@ -1,9 +1,59 @@
+const mountModel = require('../models/mount.model');
+
+
 exports.getOne = (req, res, next) => {
 	const id = req.params.id;
-	console.log(id);
+	mountModel.findById(id).then(item => {
+		res.status(201).send(item);
+	}).catch(err => res.status(500).send({
+		message: 'Error in finding this item'
+	}))
 }
 
-exports.getType = (req, res, next) => {
-	const id = req.params.id;
-	console.log(id);
+exports.getIsGround = (req, res, next) => {
+	mountModel.find({
+		isGround: true
+	})
+	.then(items => {
+		res.status(200).send(items);
+	})
+	.catch(err => res.status(500).send({
+		message: 'Error in finding these items'
+	}))
+}
+
+exports.getIsFlying = (req, res, next) => {
+	mountModel.find({
+		isFlying: true
+	})
+	.then(items => {
+		res.status(200).send(items);
+	})
+	.catch(err => res.status(500).send({
+		message: 'Error in finding these items'
+	}))
+}
+
+exports.getIsAquatic = (req, res, next) => {
+	mountModel.find({
+		isquatic: true
+	})
+	.then(items => {
+		res.status(200).send(items);
+	})
+	.catch(err => res.status(500).send({
+		message: 'Error in finding these items'
+	}))
+}
+
+exports.getIsJumping = (req, res, next) => {
+	mountModel.find({
+		isJumping: true
+	})
+	.then(items => {
+		res.status(200).send(items);
+	})
+	.catch(err => res.status(500).send({
+		message: 'Error in finding these items'
+	}))
 }
--- routes/find.route.js
+++ routes/find.route.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const findController = require('../controllers/find.controller');
 
+router.get('/isGround', findController.getIsGround);
+
+router.get('/isFlying', findController.getIsFlying);
+
+router.get('/isAquatic', findController.getIsAquatic);
+
+router.get('/isJumping', findController.getIsJumping);
+
 router.get('/:id', findController.getOne);
-
-router.get('/type/:id', findController.getType)
-
 
 module.exports = router;
